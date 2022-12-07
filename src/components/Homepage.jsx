2427ca6ce5c97f2570a5965702f5a4985b197dbc
hakimamarullah <hakimamarullah@gmail.com>
2022-12-07 22:44:16 +0700

    Start development

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display:flex;
+    align-items: center;
+    margin: 30px;
+    
+`
+const Homepage = () => {
+  return (
+    <Container>
+      <div>HOMEESSSSSSSSSSSSS</div>
+    </Container>
+  )
+}
+
+export default Homepage
